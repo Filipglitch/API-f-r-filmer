eaa8713a72bafafdce95dca3a22c79728917c65a
filip <filip.sarik@lerniastudent.se>
2026-02-08 19:42:10 +0100

    ejs

--- statisk/server.js
+++ statisk/server.js
@@ -4,7 +4,30 @@ const path = require("path");
 const app = express();
 const PORT = 5080;
 
+
 app.use(express.static(path.join(__dirname, "public")));
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
+// SSR
+app.get("/movies", async (req, res) => {
+  const response = await fetch(
+    "https://plankton-app-xhkom.ondigitalocean.app/api/movies"
+  );
+  const data = await response.json();
+
+  res.render("movies", { movies: data.data });
+});
+
+app.get("/movies/:id", async (req, res) => {
+  const response = await fetch(
+    `https://plankton-app-xhkom.ondigitalocean.app/api/movies/${req.params.id}`
+  );
+  const data = await response.json();
+
+  res.render("movie", { movie: data.data });
+});
+
 
 
 app.get("/api/movies", async (req, res) => {
--- statisk/test.js
+++ statisk/test.js
@@ -1,1 +1,14 @@
 /* console.log("Tests passed"); */
+
+const fetch = require("node-fetch");
+
+(async () => {
+  const res = await fetch("http://localhost:5080/movies");
+  const html = await res.text();
+
+  if (!html.includes("<h1>Filmer</h1>")) {
+    throw new Error("Filmsidan innehåller inte rätt titel");
+  }
+
+  console.log("Integrationstest OK: filmsidan har rätt titel");
+})();
